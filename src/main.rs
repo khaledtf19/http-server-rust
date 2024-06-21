08e54e53357ae50d48baaca2396499ec66e43290
Khaledtf Labeb <khaledtf19@gmail.com>
2024-06-21 20:25:24 +0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -14,21 +14,19 @@ fn main() {
                 stream.read(&mut buf).unwrap();
                 let req = String::from_utf8(buf.to_vec()).unwrap();
                 let req_vec = req.split(" ").collect::<Vec<&str>>();
-                let req_path = req_vec[1];
+                let req_path = req_vec[1]
+                    .split("/")
+                    .filter(|s| !s.is_empty())
+                    .collect::<Vec<_>>();
 
-                let res = match req_path {
-                    "/" => "HTTP/1.1 200 OK\r\n\r\n".to_string(),
-                    path => {
-                        if path.starts_with("/echo/") {
-                            let path_vec = path
-                                .split("/")
-                                .filter(|st| !st.is_empty())
-                                .collect::<Vec<&str>>();
-                            format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",path_vec[1].len(), path_vec[1])
-                        } else {
-                            "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
+                let res = match req_path.is_empty() {
+                    true => "HTTP/1.1 200 OK\r\n\r\n".to_string(),
+                    false => match req_path[0] {
+                        "echo" => {
+                            format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",req_path[1].len(), req_path[1])
                         }
-                    }
+                        _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
+                    },
                 };
 
                 stream.write(res.as_bytes()).unwrap();
