64249aeb80f4edcae41563ddad8f7d2679185896
Khaledtf Labeb <khaledtf19@gmail.com>
2024-06-21 20:07:03 +0300

    pass 4th stage

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,22 @@ fn main() {
                 let mut buf = [0; 100];
                 stream.read(&mut buf).unwrap();
                 let req = String::from_utf8(buf.to_vec()).unwrap();
-                println!("{:?}", req.split(" ").collect::<Vec<_>>());
                 let req_vec = req.split(" ").collect::<Vec<&str>>();
-                let res = match req_vec[1] {
-                    "/" => "HTTP/1.1 200 OK\r\n\r\n",
-                    _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+                let req_path = req_vec[1];
+
+                let res = match req_path {
+                    "/" => "HTTP/1.1 200 OK\r\n\r\n".to_string(),
+                    path => {
+                        if path.starts_with("/echo/") {
+                            let path_vec = path
+                                .split("/")
+                                .filter(|st| !st.is_empty())
+                                .collect::<Vec<&str>>();
+                            format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",path_vec[1].len(), path_vec[1])
+                        } else {
+                            "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
+                        }
+                    }
                 };
 
                 stream.write(res.as_bytes()).unwrap();
