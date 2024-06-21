c8a3388e4a0cd4b5f9db919ed10e4210e5eb6d46
Khaledtf Labeb <khaledtf19@gmail.com>
2024-06-21 16:38:00 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -7,8 +10,17 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                let response = "HTTP/1.1 200 OK\r\n\r\n";
-                stream.write(response.as_bytes()).unwrap();
+                let mut buf = [0; 100];
+                stream.read(&mut buf).unwrap();
+                let req = String::from_utf8(buf.to_vec()).unwrap();
+                println!("{:?}", req.split(" ").collect::<Vec<_>>());
+                let req_vec = req.split(" ").collect::<Vec<&str>>();
+                let res = match req_vec[1] {
+                    "/" => "HTTP/1.1 200 OK\r\n\r\n",
+                    _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+                };
+
+                stream.write(res.as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
